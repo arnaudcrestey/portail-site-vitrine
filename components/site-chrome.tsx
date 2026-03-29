2ae9650922457312605fc87a64a073843038d140
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-03-30 00:04:44 +0200

    Update site-chrome.tsx

--- components/site-chrome.tsx
+++ components/site-chrome.tsx
@@ -1,1 +1,20 @@
+"use client";
 
+import { usePathname } from "next/navigation";
+
+// Remplace ces imports par les tiens
+import { SiteHeader } from "@/components/site-header";
+import { SiteFooter } from "@/components/site-footer";
+
+export function SiteChrome({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isEntryPage = pathname === "/";
+
+  return (
+    <>
+      {!isEntryPage && <SiteHeader />}
+      {children}
+      {!isEntryPage && <SiteFooter />}
+    </>
+  );
+}
